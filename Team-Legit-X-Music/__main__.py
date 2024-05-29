049a3e181dbc430eb1733ebec95aad342b0e2914
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-29 11:57:24 +0530

    Create __main__.py

---
+++ Team-Legit-X-Music/__main__.py
@@ -0,0 +1,71 @@
+
+import asyncio
+import importlib
+import os
+
+from pyrogram import idle
+
+from Team-Legit-X-Music import (
+    ASS_ID,
+    ASS_NAME,
+    ASS_USERNAME,
+    BOT_ID,
+    BOT_NAME,
+    BOT_USERNAME,
+    LOGGER,
+    SUNAME,
+    app,
+    app2,
+    pytgcalls,
+)
+from Team-Legit-X-Music.Modules import ALL_MODULES
+
+
+async def legit_startup():
+    LOGGER.info("[•] Loading Modules...")
+    for module in ALL_MODULES:
+        importlib.import_module("Team-Legit-X-Music.Modules." + module)
+    LOGGER.info(f"[•] Loaded {len(ALL_MODULES)} Modules.")
+
+    LOGGER.info("[•] Refreshing Directories...")
+    if "downloads" not in os.listdir():
+        os.mkdir("downloads")
+    if "cache" not in os.listdir():
+        os.mkdir("cache")
+    LOGGER.info("[•] Directories Refreshed.")
+
+    try:
+        await app.send_message(
+            SUNAME,
+            f"✯ Legit ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
+        )
+    except:
+        LOGGER.error(
+            f"{BOT_NAME} failed to send message at @{SUNAME}, please go & check."
+        )
+
+    try:
+        await app2.send_message(
+            SUNAME,
+            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ᴀss ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ɴᴀᴍᴇ : {ASS_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
+        )
+    except:
+        LOGGER.error(
+            f"{ASS_NAME} failed to send message at @{SUNAME}, please go & check."
+        )
+
+    await app2.send_message(BOT_USERNAME, "/start")
+
+    LOGGER.info(f"[•] Bot Started As {BOT_NAME}.")
+    LOGGER.info(f"[•] Assistant Started As {ASS_NAME}.")
+
+    LOGGER.info(
+        "[•] \x53\x74\x61\x72\x74\x69\x6e\x67\x20\x50\x79\x54\x67\x43\x61\x6c\x6c\x73\x20\x43\x6c\x69\x65\x6e\x74\x2e\x2e\x2e"
+    )
+    await pytgcalls.start()
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(fallen_startup())
+    LOGGER.error("legit Music Bot Stopped.")
