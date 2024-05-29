fd2dca755d70fcbdc9ea766160e5db1326b5ecd5
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-29 16:20:13 +0530

    Update inline.py

--- LEGITMUSIC/help/inline.py
+++ LEGITMUSIC/help/inline.py
@@ -3,6 +3,22 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import config
 from LEGITMUSIC import BOT_USERNAME
 
+close_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="✯ ᴄʟᴏsᴇ ✯", callback_data="close")]]
+)
+
+
+buttons = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="▷", callback_data="resume_cb"),
+            InlineKeyboardButton(text="II", callback_data="pause_cb"),
+            InlineKeyboardButton(text="‣‣I", callback_data="skip_cb"),
+            InlineKeyboardButton(text="▢", callback_data="end_cb"),
+        ]
+    ]
+)
+
 pm_buttons = [
     [
         InlineKeyboardButton(
@@ -36,5 +52,30 @@ gp_buttons = [
     [
       
         InlineKeyboardButton(text=" ᴅᴇᴠᴇʟᴏᴩᴇʀ ", user_id=config.OWNER_ID),
+    ],
+]
+helpmenu = [
+    [
+        InlineKeyboardButton(
+            text="ᴇᴠᴇʀʏᴏɴᴇ",
+            callback_data="fallen_cb help",
+        )
+    ],
+    [
+        InlineKeyboardButton(text="sᴜᴅᴏ", callback_data="fallen_cb sudo"),
+        InlineKeyboardButton(text="ᴏᴡɴᴇʀ", callback_data="fallen_cb owner"),
+    ],
+    [
+        InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="fallen_home"),
+        InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
+    ],
+]
+
+
+help_back = [
+    [InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=config.SUPPORT_CHAT)],
+    [
+        InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="fallen_help"),
+        InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
     ],
 ]
