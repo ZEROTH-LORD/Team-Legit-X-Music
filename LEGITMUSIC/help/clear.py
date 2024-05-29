c40e553759bd5772db7697014fbd7a1047f54b1f
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-29 16:11:35 +0530

    Update clear.py

--- LEGITMUSIC/help/clear.py
+++ LEGITMUSIC/help/clear.py
@@ -4,7 +4,7 @@ from LEGITMUSIC.help import remove_active_chat
 
 async def _clear_(chat_id):
     try:
-        fallendb[chat_id] = []
+        legitdb[chat_id] = []
         await remove_active_chat(chat_id)
     except:
         return
